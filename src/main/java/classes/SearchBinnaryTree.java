f4bf6b059a5bb07429628d9e5e541464428f5fb2
Louis <carreno.luisdavid@gmail.com>
2023-06-19 17:37:48 -0400

    cambio del nombre del file

---
+++ src/main/java/classes/SearchBinnaryTree.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package classes;
+
+/**
+ *
+ * @author carre
+ * @param <T>
+ */
+public class SearchBinnaryTree<T> {
+    private NodoTree<T> root;
+    
+}
