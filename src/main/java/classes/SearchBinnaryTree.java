8de899b2dfde71d10d127c647bfa4262bc593958
Louis <carreno.luisdavid@gmail.com>
2023-06-19 17:54:54 -0400

    search method

--- src/main/java/classes/SearchBinnaryTree.java
+++ src/main/java/classes/SearchBinnaryTree.java
@@ -17,5 +17,22 @@ public class SearchBinnaryTree<T> {
     public NodoTree<T> getRoot() {
         return root;
     }
-    
+    public NodoTree search(NodoTree root,T element){
+        if(root == null){
+            return null;
+        }
+        else{
+            if(root.getElement() == element){
+                return root;
+            }
+            else{
+                if((int)root.getElement() > (int) element){
+                    search(root.getLeftSon(),element);
+                }
+                else{
+                    search(root.getRightSon(),element);
+                }
+            }
+        }
+    }
 }
