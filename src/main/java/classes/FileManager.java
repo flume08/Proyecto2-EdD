99c11b7af5cf5b6ec173638a80e00d893b45549c
wendala <Gonzalez-jesus@correo.unimet.edu.ve>
2023-06-21 18:42:23 -0400

    Update FileManager.java

--- src/main/java/classes/FileManager.java
+++ src/main/java/classes/FileManager.java
@@ -9,6 +9,7 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 /**
  *
@@ -17,24 +18,32 @@ import java.io.IOException;
 public class FileManager {
     
     String line = "";
+    BufferedReader reader = null;
+    PrintWriter writer = null;
       
-    public void readCSV(String path) {
+    public void readCSV(String path) {   
         try {
-            BufferedReader br = new BufferedReader(new FileReader(path));
-            
-            while (br.readLine() != null) {
-                line = br.readLine();
+            reader = new BufferedReader(new FileReader(path));
+            while (reader.readLine() != null) {
+                line = reader.readLine();
                 String[] row = line.split(",");
                 for(String data: row) {
-                    System.out.println(data); // luego aqui convertir a clase y guardar en alguna EDD.
-                    System.out.println("------------------------"); 
+                    System.out.println(data.strip()); // luego aqui convertir a clase y guardar en alguna EDD.                    
                 }
+                System.out.println("------------------------");
             }
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
+        } finally {
+            
+            try {
+                reader.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            
         }
-    }
-    
+    }   
 }
